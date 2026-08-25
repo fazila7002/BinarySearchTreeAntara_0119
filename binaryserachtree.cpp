a7e6b8fda0d304e70e8027fbfb73dc4af0029447
fazila7002 <fazilatunnisa705@gmail.com>
2026-08-25 09:59:09 +0700

    buat pilihan  ke var ch

--- binaryserachtree.cpp
+++ binaryserachtree.cpp
@@ -120,4 +120,10 @@ int main()
         cout << "5. Exit" << endl;
         cout << "\nEnter your choice (1-5) : ";
 
+        char ch;
+        cin >> ch;
+        cout << endl;
+        
+    }
+
 }
