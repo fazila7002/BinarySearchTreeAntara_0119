ba200dda0977f0d7942196d6d29cef2f4cb9048f
fazila7002 <fazilatunnisa705@gmail.com>
2026-08-25 09:54:03 +0700

    preorder, kebalikan dari inordeer

--- binaryserachtree.cpp
+++ binaryserachtree.cpp
@@ -78,5 +78,19 @@ class BinaryTree {
         }
     }
 
+        void preorder(Node* ptr) {
+
+        if (ROOT == nullptr) {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != nullptr) {
+            cout << ptr->info << " ";
+            preorder(ptr->leftChild);
+            preorder(ptr->rightChild);
+        }
+
+    }
+
 
 };
